b8fa2ae73e1a1e1cc62350ea2caba4d7ece68bea
IanCWells <ianchristopherwells@gmail.com>
2024-05-22 18:00:46 -0700

    Goes to nearest food on screen.

--- minion.js
+++ minion.js
@@ -39,6 +39,8 @@ class Minion extends Shape {
         this.xProb_adjustment = 0;
         this.zProb_adjustment = 0;
 
+        this.plane = 'x';
+
         // Transform and add the ellipsoid vertices
         for (let i = 0; i < ellipsoid.arrays.position.length; i++) {
             this.arrays.position.push(ellipsoid_transform.times(ellipsoid.arrays.position[i].to4(1)).to3());
@@ -63,7 +65,8 @@ class Minion extends Shape {
 
     movement() {
 
-        let movement_prob = Math.random()*2;
+        let movement_prob = Math.random();
+        let moveChance = Math.random();
         let movement_x = 0;
         let movement_z = 0;
 
@@ -71,33 +74,39 @@ class Minion extends Shape {
         //If z_prob > 0, there are more food items to the top than bottom of our creature
 
         //move to the right
+        let min_speed = 0.06;
 
-        if(movement_prob >= 0 && movement_prob < (0.5 + this.xProb_adjustment)){
-            movement_x = .06;
+        if(this.plane === 'x'){
+            if(movement_prob >= 0 && movement_prob < (0.5 + this.xProb_adjustment)){
+                movement_x = min_speed;
+            }
+            //move to the left
+            if(movement_prob >= (0.5 + this.xProb_adjustment) && movement_prob < 1){
+                movement_x = -min_speed;
+            }
         }
-        //move to the left
-        if(movement_prob >= (0.5 + this.xProb_adjustment) && movement_prob < 1){
-            movement_x = -0.06;
+        else
+        {
+            if(movement_prob >= 0 && movement_prob < (0.5 + this.zProb_adjustment)){
+                movement_z = min_speed;
+            }
+            //move down
+            if(movement_prob >= (0.5 + this.zProb_adjustment) && movement_prob < 1.0){
+                movement_z = -min_speed;
+            }
         }
 
-        //move up
-        if(movement_prob >= 1 && movement_prob < (1.5 + this.zProb_adjustment)){
-            movement_z = 0.06;
-        }
-        //move down
-        if(movement_prob >= (1.5 + this.zProb_adjustment) && movement_prob < 2.00){
-            movement_z = -0.06;
-        }
         let movement_vector = vec3(movement_x, 0, movement_z);
 
         return movement_vector;
     }
-    adjustProb(x_prob, z_prob){
+    adjustProb(x_prob, z_prob, plane){
         //If x_prob > 0, there are more food items to the right than left of our creature
         //If z_prob > 0, there are more food items to the top than bottom of our creature
         let prob_adjustment_factor = 1;
-        this.xProb_adjustment = x_prob*0.0001;// * prob_adjustment_factor;
-        this.zProb_adjustment = z_prob*0.0001;// * prob_adjustment_factor;
+        this.xProb_adjustment = x_prob;//*0.003;// * prob_adjustment_factor;
+        this.zProb_adjustment = z_prob;//*0.003;// * prob_adjustment_factor;
+        this.plane = plane;
     }
 
 }
