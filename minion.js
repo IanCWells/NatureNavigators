d8d8da1be64b2beae90291c880a7b73913d73b45
natewebster1 <natewebster@g.ucla.edu>
2024-06-01 13:21:52 -0700

    Size can now be modified for each species

--- minion.js
+++ minion.js
@@ -14,9 +14,9 @@ const Minion = minion_defs.Minion =
             super("positions", "normals", "texture_coords");
 
             // Create the ellipsoid body (stretched sphere)
-            this.ellipsoid = new defs.Subdivision_Sphere(2);
+            let ellipsoid = new defs.Subdivision_Sphere(2);
             // Create the circular head
-            this.circle = new defs.Subdivision_Sphere(4); // Adjust the subdivisions for the desired smoothness
+            let circle = new defs.Subdivision_Sphere(4); // Adjust the subdivisions for the desired smoothness
 
             // Define transformations
             this.radius = radius;
@@ -35,7 +35,7 @@ const Minion = minion_defs.Minion =
             this.movement_direction = vec3(0, 0, 0);
             this.speed = 1;
 
-            this.starting_energy = 10;
+            this.starting_energy = 10*2*radius;
             this.energy = this.starting_energy;
 
             this.xProb_adjustment = 0;
@@ -112,34 +112,5 @@ const Minion = minion_defs.Minion =
             this.xProb_adjustment = x_prob;//*0.003;// * prob_adjustment_factor;
             this.zProb_adjustment = z_prob;//*0.003;// * prob_adjustment_factor;
             this.plane = plane;
-        }
-        update_size(radius) {
-            this.radius = radius;
-            this.ellipsoid_transform = Mat4.translation(0, this.radius, 0)
-                .times(Mat4.scale(this.radius, this.radius*1.5, this.radius)); // Scale the sphere to form an ellipsoid
-            this.circle_transform = Mat4.translation(0, 3*this.radius, 0)
-                .times(Mat4.scale(this.radius*0.5, this.radius*0.5, this.radius*0.5))
-            this.arrays.position = [];
-            this.arrays.normal = [];
-            this.arrays.texture_coord = [];
-            this.indices = [];
-            // Transform and add the ellipsoid vertices
-            for (let i = 0; i < this.ellipsoid.arrays.position.length; i++) {
-                this.arrays.position.push(this.ellipsoid_transform.times(this.ellipsoid.arrays.position[i].to4(1)).to3());
-                this.arrays.normal.push(this.ellipsoid_transform.times(this.ellipsoid.arrays.normal[i].to4(0)).to3());
-                this.arrays.texture_coord.push(this.ellipsoid.arrays.texture_coord[i]);
-            }
-
-            // Transform and add the circle vertices
-            for (let i = 0; i < this.circle.arrays.position.length; i++) {
-                this.arrays.position.push(this.circle_transform.times(this.circle.arrays.position[i].to4(1)).to3());
-                this.arrays.normal.push(this.circle_transform.times(this.circle.arrays.normal[i].to4(0)).to3());
-                this.arrays.texture_coord.push(this.circle.arrays.texture_coord[i]);
-            }
-
-            // Combine the indices from both shapes
-            this.indices.push(...this.ellipsoid.indices);
-            const circle_offset = this.ellipsoid.arrays.position.length;
-            this.indices.push(...this.circle.indices.map(i => i + circle_offset));
         }
     }
